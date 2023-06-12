6350d9bd5dbbb900f6bf2c5fad355602ea181663
FateBrate <drek.fatih@live.com>
2023-06-12 23:56:50 +0200

    more ent

---
+++ TopLeague.Api/TopLeague.Core/Models/Referee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLeague.Core.Models
+{
+    internal class Referee:Person
+    {
+        public string League { get; set; }//later fk from League
+    }
+}
