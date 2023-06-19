081d7323dfa9ee0517ccbf86ae136fd0d7e55482
FateBrate <drek.fatih@live.com>
2023-06-19 23:57:35 +0200

    login,boott,config

--- ReactApp/top-league/src/index.tsx
+++ ReactApp/top-league/src/index.tsx
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/js/bootstrap.bundle";
