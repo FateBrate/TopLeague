510e41ec8eee497ec959b085447e64e54fa56183
FateBrate <drek.fatih@live.com>
2023-06-23 21:13:42 +0200

    ViewModels

---
+++ TopLeague.Api/TopLeague/ViewModels/AddStadiumVM.cs
@@ -0,0 +1,12 @@
+﻿namespace TopLeague.ViewModels
+{
+    public class AddStadiumVM
+    {
+        public string StadiumName { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+        public string Country { get; set; }
+        public int YearOpened { get; set; }
+        public string Club { get; set; }
+    }
+}
